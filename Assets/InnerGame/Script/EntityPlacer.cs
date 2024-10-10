9e80e08ba85241537e23303f8a11d45cb079619e
qazxsw1240 <qazxsw010528@gmail.com>
2024-10-11 03:16:47 +0900

    EntityPlacer 추가

---
+++ Assets/InnerGame/Script/EntityPlacer.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class EntityPlacer : IEntityPlacer
+{
+    private readonly Dictionary<long, EntityPlacementData> _entities;
+
+    public EntityPlacer()
+    {
+        _entities = new Dictionary<long, EntityPlacementData>();
+    }
+
+    public EntityPlacer(Dictionary<long, EntityPlacementData> entities)
+    {
+        _entities = entities;
+    }
+
+    public void Place(IEntity entity, Vector3 position)
+    {
+        if (_entities.TryGetValue(entity.Id, out EntityPlacementData data))
+        {
+            data.Position = position;
+            return;
+        }
+        data = new EntityPlacementData(entity, position);
+        _entities[entity.Id] = data;
+    }
+
+    public IEntity this[long id]
+    {
+        get
+        {
+            EntityPlacementData data = _entities.GetValueOrDefault(id);
+            if (data == null)
+            {
+                return null;
+            }
+            return data.Entity;
+        }
+    }
+
+    public void Remove(long id)
+    {
+        _entities.Remove(id);
+    }
+
+    public IEnumerator<(IEntity, Vector3)> GetEnumerator()
+    {
+        foreach (EntityPlacementData data in _entities.Values)
+        {
+            yield return (data.Entity, data.Position);
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public class EntityPlacementData
+    {
+        public EntityPlacementData(IEntity entity, Vector3 position)
+        {
+            Entity = entity;
+            Position = position;
+        }
+
+        public IEntity Entity { get; }
+
+        public Vector3 Position { get; set; }
+    }
+}
