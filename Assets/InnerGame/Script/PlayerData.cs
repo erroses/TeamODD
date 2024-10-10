db45191494e72463dd6b5f7a5a4e6e798ab64876
qazxsw1240 <qazxsw010528@gmail.com>
2024-10-11 02:53:09 +0900

    PlayerData 추가

---
+++ Assets/InnerGame/Script/PlayerData.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine.Events;
+
+public class PlayerData : IEntity
+{
+    private int _damageCount;
+    private int _attackCount;
+    private int _destroyCount;
+
+    public PlayerData(long id, string name)
+    {
+        Id = id;
+        Name = name;
+        DamageCount = 0;
+        AttackCount = 0;
+        DestroyCount = 0;
+        OnDamageCountIncrease = new UnityEvent<int>();
+        OnAttackCountIncrease = new UnityEvent<int>();
+        OnDestroyCountIncrease = new UnityEvent<int>();
+    }
+
+    public long Id { get; }
+
+    public string Name { get; }
+
+    public int DamageCount
+    {
+        get
+        {
+            return _damageCount;
+        }
+        set
+        {
+            _damageCount = value;
+            OnDamageCountIncrease.Invoke(_damageCount);
+        }
+    }
+
+    public int AttackCount
+    {
+        get
+        {
+            return _attackCount;
+        }
+        set
+        {
+            _attackCount = value;
+            OnAttackCountIncrease.Invoke(_attackCount);
+        }
+    }
+
+    public int DestroyCount
+    {
+        get
+        {
+            return _destroyCount;
+        }
+        set
+        {
+            _destroyCount = value;
+            OnDestroyCountIncrease.Invoke(_destroyCount);
+        }
+    }
+
+    public UnityEvent<int> OnDamageCountIncrease { get; }
+
+    public UnityEvent<int> OnAttackCountIncrease { get; }
+
+    public UnityEvent<int> OnDestroyCountIncrease { get; }
+}
