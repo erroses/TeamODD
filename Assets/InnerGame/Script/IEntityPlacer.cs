881880b561683e0ec4351c511793ad4d15d0d0c7
qazxsw1240 <qazxsw010528@gmail.com>
2024-10-11 01:48:59 +0900

    IEntityPlacer 추가

---
+++ Assets/InnerGame/Script/IEntityPlacer.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+using UnityEngine;
+
+public interface IEntityPlacer : IEnumerable<(IEntity, Vector3)>
+{
+    public void Place(IEntity entity, Vector3 position);
+
+    public IEntity this[long id] { get; }
+}
