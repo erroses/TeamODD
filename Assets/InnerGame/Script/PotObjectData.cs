0c0b9fa512e9171e53db0976936421fa2c39678c
qazxsw1240 <qazxsw010528@gmail.com>
2024-10-11 02:53:35 +0900

    PotObjectData 추가

---
+++ Assets/InnerGame/Script/PotObjectData.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine.Events;
+
+public class PotObjectData : IEntity
+{
+    private int _healthPoint;
+
+    public PotObjectData(long id, string name, int healthPoint)
+    {
+        Id = id;
+        Name = name;
+        _healthPoint = healthPoint;
+    }
+
+    public long Id { get; }
+
+    public string Name { get; }
+
+    public int HealthPoint
+    {
+        get
+        {
+            return _healthPoint;
+        }
+        set
+        {
+            int previous = _healthPoint;
+            _healthPoint = value;
+            OnHealthPointChange.Invoke(previous, _healthPoint);
+        }
+    }
+
+    public UnityEvent<int, int> OnHealthPointChange { get; }
+}
