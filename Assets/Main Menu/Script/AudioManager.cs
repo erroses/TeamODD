e19d97b0f2244893718e95645dea930da5e28d36
erroses <118429459+erroses@users.noreply.github.com>
2024-10-12 21:21:59 +0900

    메인화면 배경음악 추가
    Change:
    - 배경음악을 메인화면으로 이동

--- Assets/Main Menu/Script/AudioManager.cs
+++ Assets/Main Menu/Script/AudioManager.cs
@@ -2,10 +2,25 @@ using UnityEngine;
 
 public class AudioManager : MonoBehaviour
 {
+    private static AudioManager instance;
     private AudioSource audioSource;
     public AudioClip clip;
 
     private bool isPaused = false;
+
+    private void Awake()
+    {
+        // Singleton pattern
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // 이 오브젝트를 씬 전환 시 파괴되지 않도록 설정
+        }
+        else
+        {
+            Destroy(gameObject); // 이미 존재하는 인스턴스가 있을 경우 현재 오브젝트를 파괴
+        }
+    }
 
     private void Start()
     {
