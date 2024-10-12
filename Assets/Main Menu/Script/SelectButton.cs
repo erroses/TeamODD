f87b3b4d026bf2cdc00cad3159b9f73dd0ee601b
erroses <118429459+erroses@users.noreply.github.com>
2024-10-12 20:23:27 +0900

    시작화면 구현
    Add:
    - 시작화면 구현

---
+++ Assets/Main Menu/Script/SelectButton.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectButton : MonoBehaviour
+{
+    public void SelectStart()
+    {
+        SceneManager.LoadScene("Internal");
+    }
+
+    public void SelectOption()
+    {
+        // 옵션 띄우기
+    }
+
+    public void SelectExit()
+    {
+        // 에디터에서 실행 중인지 확인 (에디터에서는 종료되지 않기 때문에 메시지를 띄움)
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #else
+        Application.Quit(); // 실제 빌드된 게임에서 실행 시 게임을 종료
+    #endif
+    }
+}
