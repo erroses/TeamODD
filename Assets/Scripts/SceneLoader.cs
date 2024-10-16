e5485a566e05122ce3d0623e25bc474dcbb323a9
qazxsw1240 <qazxsw010528@gmail.com>
2024-10-17 07:10:01 +0900

    SceneLoader 개선

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,9 +1,48 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace GameJam.Project
 {
     public class SceneLoader : MonoBehaviour
     {
-        
+        private static void LoadScene(string sceneName, LoadSceneMode mode)
+        {
+            SceneManager.LoadScene(sceneName, mode);
+        }
+
+        public static SceneLoader Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance)
+            {
+                Destroy(this);
+            }
+            else
+            {
+                DontDestroyOnLoad(this);
+                Instance = this;
+            }
+        }
+
+        public void LoadScene(string sceneName)
+        {
+            LoadScene(sceneName, LoadSceneMode.Single);
+        }
+
+        public void LoadSceneAdditive(string sceneName)
+        {
+            LoadScene(sceneName, LoadSceneMode.Additive);
+        }
+
+        public void UnloadScene()
+        {
+            UnloadScene(SceneManager.GetActiveScene().name);
+        }
+
+        public void UnloadScene(string sceneName)
+        {
+            SceneManager.UnloadSceneAsync(sceneName);
+        }
     }
 }
